c8e1cf055629f423562a93d68dd91b35773baba8
ErfanMo77 <43669965+ErfanMo98@users.noreply.github.com>
2021-11-27 20:34:22 +0330

    added new images.

--- src/mock/data.js
+++ src/mock/data.js
@@ -20,25 +20,25 @@ export const aboutData = {
   img: 'Me.jpg',
   paragraphOne: 'I am a creative, multi-disciplinary computer engineer.',
   paragraphTwo:
-    'My main interests lie in video games. I love designing, developing, and playing video games.\
-   Specifically, I am interested in real-time graphics and developing optimized tools and engines to build games.',
+    'Video games are my main interest. I enjoy designing, developing, and playing them.\
+     In particular, I am fascinated by real-time graphics and developing optimized tools and engines to build games.',
   paragraphThree:
     'In the future, I would like to contribute to building innovative and immersive video games by\
    finding efficient methods to solve the problems in making video games.',
-  resume: '../images/cv.pdf',
+  resume: '../images/cv2.pdf',
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project1.png',
+    img: 'Syndra.png',
     title: 'Syndra Engine',
     info: 'Syndra is an early-stage 3D game engine.',
     info2:
-      "Syndra is my bachelor's thesis project, where I wanted to study how to build 3D game engines.\
+      "Syndra is my bachelor's thesis project, where I wanted to study how to create 3D game engines.\
        I've been using C++ and OpenGL API to develop Syndra for the Windows platform.\
-       My main motivation for making syndra was to learn new algorithms and techniques used mainly in real-time rendering.",
+       I made Syndra mainly to learn new algorithms and techniques used primarily in real-time rendering.",
     url: 'https://www.youtube.com/watch?v=PlPZ7A7ZCdA',
     repo: 'https://github.com/ErfanMo77/Syndra',
   },
@@ -59,7 +59,7 @@ export const projectsData = [
     title: 'Pentago',
     info: 'Pentago is a board game usually played with two players. You can also play with AI.',
     info2:
-      'I implemented this project in Java using mini-max tree and alpha-beta pruning to find the next best move. The UI is made with JavaFX.',
+      'I implemented this project in Java. I used mini-max tree and alpha-beta pruning to find the next best move. The UI is made with JavaFX.',
     url: '',
     repo: 'https://github.com/ErfanMo77/Pentago_mini-max',
   },
@@ -71,21 +71,21 @@ export const artProjectsData = [
     id: nanoid(),
     img: 'Nature.jpg',
     title: 'The Road',
-    info: 'This scene was created as my submission in a weekly environment art competition.',
+    info: 'I created this scene as my entry in a weekly environment art competition.',
     info2:
-      'It was created using "Quixel" materials and assets in Unreal Engine 4. I did the lighting and post-processing and also the setting of the scene.\
+      'It was created was created with Unreal Engine 4 and "Quixel" materials. I did the lighting and post-processing and also the setting of the scene.\
     A subsurface scattering shader was used to create realistic tree leaves.',
     url: require('../images/Nature.jpg'),
     repo: '',
   },
   {
     id: nanoid(),
-    img: 'GrassCar',
+    img: 'GrassCar2',
     title: 'Grass Car',
     info: "This scene was created in Blender following Derek Elliott's tutorials on Youtube.",
     info2:
-      'This was done by using particle system of blender and it was rendered with Cylcles renderer.',
-    url: require('../images/GrassCar.png'),
+      'The grass was created using particle system of blender. It was rendered with Cycles renderer.',
+    url: require('../images/GrassCar2.png'),
     repo: '',
   },
   {
@@ -94,20 +94,20 @@ export const artProjectsData = [
     title: 'Shack',
     info: 'I created this scene in Blender.',
     info2:
-      "I did the modeling, lighting, and rendering of this scene following Grant Abbitt's Blender tutorials on Youtube.\
-     it was one of the first scenes that I created in Blender as I was learning and practising 3D modeling.",
+      "This is one of the first scenes I built in Blender as I was learning 3D modeling.\
+       I created it following Grant Abbitt's Blender tutorials on Youtube.",
     url: require('../images/shack.png'),
     repo: '',
   },
   {
     id: nanoid(),
-    img: 'well.png',
+    img: 'well2.png',
     title: 'Well',
     info: 'This scene was created in Blender.',
     info2:
       'It was also modeled by following Grant Abbitt\'s "Well" tutorial on Youtube.\
     Many modifiers like Lattice tool in Blender was used to create the well.',
-    url: require('../images/well.png'),
+    url: require('../images/well2.png'),
     repo: '',
   },
 ];
